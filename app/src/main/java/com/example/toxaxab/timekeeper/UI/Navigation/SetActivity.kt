c48845ed56ba7b25110f84f741c6e66587e2c430
Anton Mikhailenko <toxaxab27@gmail.com>
2019-03-06 15:39:47 +1000

    Add first activity

---
+++ app/src/main/java/com/example/toxaxab/timekeeper/UI/Navigation/SetActivity.kt
@@ -0,0 +1,14 @@
+package com.example.toxaxab.timekeeper.UI.Navigation
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.toxaxab.timekeeper.R
+
+class SetActivity: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+}
