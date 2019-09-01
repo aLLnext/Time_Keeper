7e64e23f469eea5061a2dd6ce0001e75e8e1263f
Anton <toxaxab27@gmail.com>
2019-09-01 21:27:28 +1000

    Fix fonts

--- app/src/main/java/com/timekeeper/Fragments/DashboardFragment.kt
+++ app/src/main/java/com/timekeeper/Fragments/DashboardFragment.kt
@@ -1,15 +1,22 @@
 package com.timekeeper.Fragments
 
+import android.graphics.Typeface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.timekeeper.R
+import kotlinx.android.synthetic.main.fragment_dashboard.view.*
 
 class DashboardFragment: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val v = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val MLight = Typeface.createFromAsset(activity!!.assets, "fonts/Montserrat-Light.ttf")
+        val MMedium = Typeface.createFromAsset(activity!!.assets, "fonts/Montserrat-Medium.ttf")
+        v.titlepageDashboard!!.typeface = MMedium
+        v.subtitlepageDashboard.typeface = MLight
+        return v
     }
 }
--- app/src/main/java/com/timekeeper/Fragments/SettingsFragment.kt
+++ app/src/main/java/com/timekeeper/Fragments/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.timekeeper.Fragments
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.preference.PreferenceFragment
 import androidx.fragment.app.Fragment
@@ -7,9 +8,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.timekeeper.R
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.fragment_settings.view.*
 
 class SettingsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val v = inflater.inflate(R.layout.fragment_settings, container, false)
+        val MLight = Typeface.createFromAsset(activity!!.assets, "fonts/Montserrat-Light.ttf")
+        val MMedium = Typeface.createFromAsset(activity!!.assets, "fonts/Montserrat-Medium.ttf")
+        v.titlepageSettings!!.typeface = MMedium
+        v.subtitleSettings.typeface = MLight
+        return v
     }
 }
